954be1526aa66902db7f1489edefa95ddf6fac7e
Shreya sinha <53377209+shreya-12345-sinha@users.noreply.github.com>
2022-05-25 00:04:54 +0530

    Add files via upload

---
+++ Drives/about.php
@@ -0,0 +1,245 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+<title>Drives :: About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tillage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstarp-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstarp-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,800,700,600' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+	 new WOW().init();
+</script>
+</head>
+<body>
+	<!-- banner -->
+	<div class="banner a-banner">
+		<!-- container -->
+		<div class="container">
+			<div class="header">
+				<div class="head-logo">
+					
+				</div>
+				<div class="top-nav">
+					<span class="menu"><img src="images/menu.png" alt=""></span>
+					<ul class="nav1">
+						<li class="hvr-sweep-to-bottom">
+								<a href="../Homepage/index.php">UIC-CU<i>Goto Placement Homepage</i></a>
+							</li>
+						<li class="hvr-sweep-to-bottom">
+								<a href="index.php">Home<i>Get the Overview of the Site</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom active">
+								<a href="about.php">About<i>About Us and our Chief Architects</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="products.php">Campus Drives<i>Campus Drives</i></a>
+							</li>
+							
+							<li class="hvr-sweep-to-bottom">
+								<a href="mail.php">Mail Us<i>Get in Touch with us Instantly</i></a>
+							</li>
+							<div class="clearfix"></div>
+						<div class="clearfix"> </div>
+					</ul>
+					<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.nav1" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+						<!-- /script-for-menu -->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner -->
+	<!-- about -->
+	<div class="about-top">
+		<!-- container -->
+		<div class="container">
+			<div class="about-info wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+				<h3>A few words about us</h3>
+				<h5>This is what tells about who we are and about Pioneers of the Institution </span></h5>
+			</div>
+			<div class="banner-bottom-grids a-banner-bottom-grids">
+				<div class="col-md-5 banner-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<img src="images/a1.jpg" alt="">
+				</div>
+				<div class="col-md-7 a-banner-bottom-text">
+					<div class="jumbotron banner-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					  <h3>UIC Placement and Training Department</h3>
+						<h5></span></h5>
+						<p>Chandigarh University (CU) is a private university located in Mohali, Punjab, India.The university was established on 10 July 2012 by an act of Punjab State Legislature.It is recognized by University Grants Commission under Section with the right to confer degrees as per Section of the UGC Act, 1956. Chandigarh University has been accredited 'A+' by NAAC in 2019. Chandigarh University is ranked among the Leading & Best Educational Institution in North India. CU is constantly ranked among the top institutions of Punjab for academic excellence, innovative pedagogy, research, and placements by reputed national and international accreditation bodies and media houses.
+Chandigarh University has become an indisputable leader in campus placements. The top-tier multinationals that visit us repeatedly stand as a testimony to the fact that CU has created a niche as the 'University with Best Placements' in India. Established with a vision to empower students with the fervour to learn and excel, Chandigarh University is the leading and Best University in India offering inspirational learning to foster the students to lead the professional world. From Engineering to Management, Commerce to Animation, Pharma Sciences to Media & Journalism, Chandigarh University (CU) offers a plethora of choicest courses and opportunities. Chandigarh University is ranked 7th in Top 25 Young Universities of India and 10th among Top 50 Private State Universities of India. The University is also Limca Book of Records holder for the Highest Number of Companies for Campus Placements in India in a single academic year. The University has also been ranked 7th among the Top 25 Socioversities of India by Careers 360 for its popularity on various social media platforms.
+						</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about -->
+	<!-- about-bottom -->
+	<div class="about-bottom">
+		<!-- container -->
+		<div class="container">
+			<h3 class="wow fadeInRight animated" style="visibility: visible;">A New Generation of CU</h3>
+			<div class="about-bottom-grids">
+				<div class="col-md-6 about-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<h4>What We Offer?</h4>
+					<p>The Institute offers bachelors degree courses in various streams like BCA, MCA, B.SC (COMPUTER SCIENCE), B.tech, MBA and many more.
+					</p>
+				</div>
+				<div class="col-md-6 about-bottom-left about-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<h4>We Promote</h4>
+					<p>We wish to bring to your kind notice that for promoting the campus employment opportunities for students of the Engineering Colleges located in the rural areas, we have formed a joint understanding with many other Institutions on full commitment basis. This association is called “ACCESS” - ASSOCIATION OF ENGINEERING COLLEGES FOR THE JOINT CAMPUS EMPLOYMENT SERVICES AND SELECTIONS, with its headquarters in our Campus. Through this association we can organize pool recruitment drives with large footfall as per the requirement.
+					<span>We strongly believe that the Placement process is not just a yearly affair, but is an on-going relationship between an Institute and the organization. I would like to thank the industry for its kind patronage and successful associations with us year after year and look forward to similar 'tie-ups' in future. </span>
+					</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about-bottom -->
+	<!-- team -->
+	<div class="team">
+		<!-- container -->
+		<div class="container">
+			<div class="team-info">
+				<h3 class="wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">Meet Our Team</h3>
+				<div class="team-grids">
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b7.jpeg" alt="">
+						<h6>Manisha Malhotra</h6>
+						<p> HOD UIC Department</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b10.jpg" alt="">
+						<h6>Name</h6>
+						<p>Designation</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b8.jpg" alt="">
+						<h6>Name</h6>
+						<p> Designation</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b9.jpeg" alt="">
+						<h6>Ankush Kapoor</h6>
+						<p>Placement Coordinator</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //team -->
+	<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="col-md-6 footer-left  wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						<li>
+							<a href="products.php">Campus Drives</a>
+						</li>
+						<li>
+							<a href="../Gallery/index.html">Gallery</a>
+						</li>
+						<li>
+							<a href="mail.php">Mail Us</a>
+						</li>
+					</ul>
+					<form>
+						<input type="text" placeholder="Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+				</div>
+				<div class="col-md-3 footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<h3>Address</h3>
+					<div class="address">
+						<p>NH95 Chandigarh-Ludhiana Highway,
+							<span>Gharuan, Mohali, Punjab, India</span>
+						</p>
+					</div>
+					<div class="phone">
+						<p>18002571800</p>
+					</div>
+				</div>
+				<div class="col-md-3 footer-right  wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<h6 style="color: white">University Institute of Computing, Chandiagrh University</h6>
+					<h4>
+						<a href="../Homepage/index.php" style="color: white">UIC Placement Management System</a>
+					</h4>
+					<p></p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //footer -->
+		<div class="copyright">
+			<!-- container -->
+			<div class="container">
+				<div class="copyright-left wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					 <p>Copyright &copy; 2022 UIC-CU | Developed by
+              <a href="http://znumerique.azurewebsites.net" target="_parent">Shreya Sinha</a>
+					</p>
+				</div>
+				<div class="copyright-right wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="#" class="twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter facebook"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter chrome"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter pinterest"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter linkedin"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter dribbble"> </a>
+						</li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+</body>
+</html>
